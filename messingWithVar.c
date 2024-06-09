f9303158ac24c0b99dfab50bd430510f2af838cb
p3ntag01 <p3ntag01@proton.me>
2024-06-09 21:56:49 +0545

    Found out I can do process steps within the print statement

---
+++ messingWithVar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int myVar = 20;
+    int newVar ;
+
+    printf("The value of myVar is %d\n",myVar);
+
+    newVar = myVar ;
+
+    int daSum ;
+
+    printf("We created a newVar\n");
+    printf("newVar equals %d and daSum equals %d\n",newVar,daSum=newVar+myVar);
+
+    // As always we can do the process steps within the print statement too :)
+
+}
