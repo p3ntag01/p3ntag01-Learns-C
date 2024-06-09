df96abc86d90796895d3db46d45b70d5820543ac
p3ntag01 <p3ntag01@proton.me>
2024-06-09 22:11:27 +0545

    Some cool assignment trick ?

---
+++ MultiAssignment.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // In this one we learn some interesting stuff
+
+    int a , b , c ;
+
+    // You may have seen this stuff
+    // But have you seen this 
+
+    a = b = c = 30;
+
+    printf("Now we print all 3 of them , \n a = %d ,b = %d ,c= %d .",a,b,c);
+
+    printf("\n");
+
+}
